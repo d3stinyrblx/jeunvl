8764462b5839e1fbd584c1ec64674393f821eceb
D3stinyRBLX <crooyzofficial@gmail.com>
2025-02-07 16:31:54 +0800

    Update 4pJyY63mqjed1nmY6CwN.lua

--- secrets/files/roblox/scripts/swordpower/4pJyY63mqjed1nmY6CwN.lua
+++ secrets/files/roblox/scripts/swordpower/4pJyY63mqjed1nmY6CwN.lua
@@ -1,34 +1,11 @@
-local Players = game:GetService("Players")
-local StarterGui = game:GetService("StarterGui")
-local InsertService = game:GetService("InsertService")
+local player = game.Players.LocalPlayer
+local gearID = 121946387
 
-local function giveSword(player)
-    local sword = InsertService:LoadAsset(422024417):FindFirstChildOfClass("Tool")
-
-    if sword then
-        sword.Parent = player.Backpack
-
-        local function onHit(otherPart)
-            local character = otherPart.Parent
-            if character and character:FindFirstChild("Humanoid") then
-                local humanoid = character.Humanoid
-                humanoid.Health = 0
-            end
-        end
-
-        sword.Handle.Touched:Connect(onHit)
+local function giveGear()
+    local tool = game:GetService("InsertService"):LoadAsset(gearID):GetChildren()[1]
+    if tool then
+        tool.Parent = player.Backpack
     end
 end
 
-Players.PlayerAdded:Connect(function(player)
-    player.Chatted:Connect(function(message)
-        if message:lower() == "i had the power" then
-            giveSword(player)
-            StarterGui:SetCore("SendNotification", {
-                Title = "Sword Given";
-                Text = "You have received the power!";
-                Duration = 3;
-            })
-        end
-    end)
-end)
+giveGear()
